ee848cf15f580bc75547ab00653c2f4609bb52ad
Sidney N <snibo13@gmail.com>
2023-07-21 13:46:20 -0700

    v1.0

--- keyboard.cpp
+++ keyboard.cpp
@@ -7,36 +7,29 @@ uint32_t checkBit(uint16_t value, uint8_t pos)
 
 void keyboard_task()
 {
-    hid_keyboard_report_t keyboard_report = {0};
+    uint8_t keycode[NUM_KEYS] = {0};
     load_to_register();
     uint8_t *keyboard_state = new uint8_t[NUM_REGISTERS];
     memset(keyboard_state, 0, NUM_REGISTERS * sizeof(uint8_t));
     read_register(keyboard_state);
-    // printf("%d\n\n", keyboard_state);
     uint32_t pressed;
     bool key_pressed = false;
+    uint32_t indx = 0;
     for (int r = 0; r < NUM_REGISTERS; r++)
     {
-        // printf("Register %d\n", r);
         for (int i = 0; i < 8; i++)
         {
             pressed = checkBit(keyboard_state[r], i);
             if (pressed)
             {
-                keyboard_report.keycode[0] |= keymap[r][i];
+                keycode[indx] |= keymap[r][i];
+                indx++;
                 key_pressed = true;
             }
-            // printf("%d\t", temp);
         }
-        // printf("\n");
     }
 
-    // printf("\n");
-
     delete[] keyboard_state;
-    uint8_t keycode[8] = {0};
-    keycode[0] = HID_KEY_A;
-    keycode[1] = HID_KEY_B;
     // skip if hid is not ready yet
     if (!tud_hid_ready())
         return;
@@ -47,31 +40,6 @@ void keyboard_task()
     {
         // uint8_t keycode[6] = {0};
         // keycode[0] = HID_KEY_A;
-
-        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
-        has_keyboard_key = true;
-    }
-    else
-    {
-        // send empty key report if previously has key pressed
-        if (has_keyboard_key)
-            tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
-        has_keyboard_key = false;
-    }
-}
-
-static void send_hid_report(uint8_t report_id, uint32_t btn)
-{
-    // skip if hid is not ready yet
-    if (!tud_hid_ready())
-        return;
-    // use to avoid send multiple consecutive zero report for keyboard
-    static bool has_keyboard_key = false;
-
-    if (btn)
-    {
-        uint8_t keycode[6] = {0};
-        keycode[0] = HID_KEY_A;
 
         tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
         has_keyboard_key = true;
